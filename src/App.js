15fad3e166cd37251781a06ad3a2255d204072c5
bhague1281 <bhague1281@gmail.com>
2019-06-04 20:39:45 -0700

    working google calendar example

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ApiCalendar from 'react-google-calendar-api';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class DoubleButton extends React.Component {
+    constructor(props) {
+      super(props);
+      this.handleItemClick = this.handleItemClick.bind(this);
+      this.createEvent = this.createEvent.bind(this);
+    }
+    
+    handleItemClick(event, name) {
+      if (name === 'sign-in') {
+        ApiCalendar.handleAuthClick();
+      } else if (name === 'sign-out') {
+        ApiCalendar.handleSignoutClick();
+      }
+    }
+
+    createEvent() {
+      const eventFromNow = {
+        summary: "Poc Dev From Now",
+        time: 480,
+      };
+   
+    ApiCalendar.createEventFromNow(eventFromNow)
+      .then((result) => {
+        console.log(result);
+      })
+       .catch((error) => {
+         console.log(error);
+      });
+    }
+
+    render() {
+      return (
+        <div>
+          <button
+                onClick={(e) => this.handleItemClick(e, 'sign-in')}
+            >
+              sign-in
+            </button>
+            <button
+                onClick={(e) => this.handleItemClick(e, 'sign-out')}
+            >
+              sign-out
+            </button>
+            <button onClick={this.createEvent}>
+              Create Event
+            </button>
+        </div>
+      );
+    }
 }
-
-export default App;
